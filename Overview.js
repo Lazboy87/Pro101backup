1b06cb495429467843ecee2e9b2450d5d4156c8a
Hans Maast <hfmaast@gmail.com>
2019-05-29 13:49:14 +0200

    Added blueprint for drag and drop

--- Overview.js
+++ Overview.js
@@ -6,21 +6,14 @@ const compCon = document.getElementById('CompletedContainer');
 
 const form = document.getElementById('projectform');
     
-
-
     upcCon.style.display ="none";
     impCon.style.display="block";
     compCon.style.display="none";
     form.style.display="none";
 
-
-
 var inProgressPro=[];
 var upcomPro=[];
 var completePro=[];
-
-
-
 
 function showupc() 
 {
@@ -100,8 +93,7 @@ document.getElementById("IpProject1").appendChild(divpro);
 
 }
     
-    
-    
+
 if(type == "upcoming"){
     
 var upcs= " Upcoming";  
@@ -112,32 +104,93 @@ document.getElementById("Feedup1").appendChild(feedpro);
     
 document.getElementById("UpProject1").appendChild(divpro);
  
-       namepro={name:namepro,Type:upcs,}
+    namepro={name:namepro,Type:upcs,};
     upcomPro.push(namepro);
     
     
     closeform();   
 }   
-    
-    
- 
-  
-    
-    
-console.table(inProgressPro);
-    console.table(upcomPro);
 
+console.table(inProgressPro);
+console.table(upcomPro);
 
 }
 
 
 /* Drag and drop functions and listeners */
 
-
-
 const dragpro=document.getElementByClass("projectdiv");
 
-const dragtab=document.get
+const dragtab=document.get;
+
+//---> Try to implement this
+// The dragged item gets stored in dragged
+var dragged;
+
+//Eventes fires on the dargable target
+document.addEventListener("drag", function(e){
+    console.log("drag");
+}, false);
+
+//Dragstart
+document.addEventListener("dragstart", (e)=>{
+    //Store a reference on the dragged item
+    dragged = e.target;
+    //Make it more transparent on dragstart
+    e.target.style.opacity = .5;
+
+    console.log("drag start");
+}, false);
+
+//Dragend
+document.addEventListener("dragend", (e)=>{
+    //Reset transparency
+    e.target.style.opacity= "";
+
+    console.log("drag end");
+}, false);
+
+//Dragover
+document.addEventListener("dragover", (e)=>{
+    //Prevent default to allow drop
+    e.preventDefault();
+    console.log("drag over");
+}, false)
+
+//Dragenter
+document.addEventListener("dragenter", (e)=>{
+    //Highlight drop target when draggable element enters it
+    if (e.target.className == "dropZone") {
+        e.target.style.background = "grey";
+        console.log("drag enter");
+    }
+
+}, false)
+
+//Dragleave
+document.addEventListener("dragleave", (e)=>{
+    //Resets background of drop target whe element leaves it
+    if (e.target.className == "dropZone") {
+        e.target.style.background = "";
+        console.log("drag leave");
+    }
+
+}, false);
+
+//DROP
+document.addEventListener("drop", (e)=>{
+    //Prevent default action
+    e.preventDefault();
+    //Move dragged element to the selected drop target
+    if (e.target.className == "dropZone") {
+        e.target.style.background = "";
+        dragged.parentNode.removeChild(dragged);
+        e.target.appendChild(dragged);
+
+        console.log("drop");
+    }
+
+}, false)
 
 
 
